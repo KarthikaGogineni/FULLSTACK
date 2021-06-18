1099b70aa582bc522393d5cf2bc401c262a882ae
KarthikaGogineni <73654045+KarthikaGogineni@users.noreply.github.com>
2021-06-18 11:03:29 +0530

    Add files via upload

--- part7/part7.1-7.21/country-hook/src/components/Country.js
+++ part7/part7.1-7.21/country-hook/src/components/Country.js
@@ -1,1 +1,28 @@
+import React from 'react'
 
+const Country = ({ country }) => {
+    if (!country) {
+      return null
+    }
+
+    if (country.length === 0) {
+        return (
+            <div>
+            not found...
+            </div>
+        )
+    }
+
+    const  cObject = country[0]
+
+    return (
+    <div>
+        <h3>{ cObject.name} </h3>
+        <div>capital { cObject.capital} </div>
+        <div>population { cObject.population}</div>
+        <img src={ cObject.flag} height='100' alt={`flag of ${countryObj.name}`}/>
+    </div>
+    )
+  }
+
+  export default Country
